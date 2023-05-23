63595122a49a7bc820dcb754ed7e7179c252286c
ombeltran <ombeltranduk@gmail.com>
2023-05-22 23:10:46 -0500

    Second commit pesonal portfolio wit update contact section

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let text = document.getElementById("overwrite-text");
-let words = ["I am developer", "I am designer"];
+let words = ["I am developer.", "I am designer.", "I am freelancer.", "I am engineer."];
 let indice = 0;
 
 setInterval(function() {
     text.innerHTML = words[indice];
     indice = (indice + 1) % words.length;
-},5000);
+},3000);
