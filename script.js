321fc30cd6f7d7fa2e528d7f92a5edb840396b37
ombeltran <ombeltranduk@gmail.com>
2023-05-26 21:46:05 -0500

    Third commit personal portfolio picture home and images portfolio section

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let text = document.getElementById("overwrite-text");
-let words = ["I am developer.", "I am designer.", "I am freelancer.", "I am engineer."];
+let words = ["Designer", "Freelancer", "Engineer"];
 let indice = 0;
 
 setInterval(function() {
     text.innerHTML = words[indice];
     indice = (indice + 1) % words.length;
-},3000);
+},1500);
