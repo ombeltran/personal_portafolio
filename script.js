f96d71ff59d87e404dc6adcf25675486ff0a046a
ombeltran <ombeltranduk@gmail.com>
2023-05-27 23:50:26 -0500

    Fifth commit personal portfolio second responsive ajust

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ setInterval(function() {
     text.innerHTML = words[indice];
     indice = (indice + 1) % words.length;
 },1500);
+
+
+// Manage menu bar
+const toggleMenu = document.getElementById("toggle-menu");
+const navMenu = document.querySelector("nav");
+let miSize = 0;
+
+toggleMenu.addEventListener('click', () => {
+    navMenu.style.visibility = 'visible';
+    miSize = 1;
+});
+
+navMenu.addEventListener('click',() => {
+    if (miSize === 1){
+        navMenu.style.visibility = 'hidden';
+    }
+});
